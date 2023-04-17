eeb55941968376043740e324b172a3bf8141ae75
Rafael Maestre Del Río <a20madera@iesgrancapitan.org>
2023-04-17 13:35:37 +0200

    Solucion de Modelos subida

---
+++ Thunderbank.Models/Responsable.cs
@@ -0,0 +1,15 @@
+﻿namespace Thunderbank.Models
+{
+    public class Responsable
+    {
+        public int Id { get; set; }
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public int idUsuario { get; set; }
+
+    }
+}
