1e8a66c7a274f41066f8ad5460fb9b43d6f6c5ed
Rafael Maestre Del Río <a20madera@iesgrancapitan.org>
2023-04-27 11:18:51 +0200

    Cambio de Menú y agregada Funcionalidad Crear Cuentas Funcional

---
+++ ThunderBank.Main/ThunderBank.Services/Repositorios/RepositorioCuenta.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using ThunderBank.Models;
+using ThunderBank.Services.Interfaces;
+using ThunderBank.Services.SQL;
+
+namespace ThunderBank.Services.Repositorios
+{
+    public class RepositorioCuenta : IRepositorioCuenta
+    {
+        private readonly SqlConfiguration _configuration;
+
+        public RepositorioCuenta(SqlConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected SqlConnection DbConnection()
+        {
+            return new SqlConnection(_configuration.ConnectionString);
+        }
+        public async Task Crear(Cuenta cuenta)
+        {
+            var db = DbConnection();
+            var sql = @"INSERT INTO Cuenta(numero,saldo,fechaApertura,tipo,idCliente)
+                VALUES (@Numero,@Saldo,@FechaApertura,@Tipo,1002)";
+            await db.QuerySingleAsync(sql, cuenta);
+        }
+    }
+}
