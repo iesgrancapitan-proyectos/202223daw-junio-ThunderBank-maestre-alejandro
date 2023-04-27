158529315e9a2f715a21a788a72d2dfb1410a9aa
Rafael Maestre Del Río <a20madera@iesgrancapitan.org>
2023-04-27 08:57:23 +0200

    Subida de proyecto v2

---
+++ ThunderBank.Main/ThunderBank.Main/Controllers/ClienteController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThunderBank.Models;
+using ThunderBank.Services.Interfaces;
+
+namespace ThunderBank.Main.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly IRepositorioCliente _repositorioCliente;
+
+        public ClienteController(IRepositorioCliente repositorioCliente)
+        {
+            this._repositorioCliente = repositorioCliente;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cliente);
+            }
+            await _repositorioCliente.Crear(cliente);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ThunderBank.Main/ThunderBank.Services/Interfaces/IRepositorioCliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThunderBank.Models;
+
+namespace ThunderBank.Services.Interfaces
+{
+    public interface IRepositorioCliente
+    {
+        Task Crear(Cliente cliente);
+    }
+}
